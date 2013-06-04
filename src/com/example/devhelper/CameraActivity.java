3598fb6bed31cabeb7a9d8aeda624b43794c603f
Li Gang <gli@eee168.com>
2013-06-04 19:02:07 +0800

    add save animation for camera

--- src/com/example/devhelper/CameraActivity.java
+++ src/com/example/devhelper/CameraActivity.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.util.List;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
 import android.view.Surface;
@@ -12,14 +16,30 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceHolder.Callback;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 
-public class CameraActivity extends Activity implements Callback {
+public class CameraActivity extends Activity implements Callback, AnimationListener {
 
 	private SurfaceView preview;
 
 	private SurfaceHolder mSurfaceHolder;
 
 	private Camera mCamera;
+
+	private ImageView image, tag;
+
+	private AnimationSet as;
+	private TranslateAnimation ta;
+
+	private ScaleAnimation sa;
+
+	private boolean isAnimating = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +48,31 @@ public class CameraActivity extends Activity implements Callback {
 		preview = (SurfaceView) findViewById(R.id.camera_preview);
 		mSurfaceHolder = preview.getHolder();
 		mSurfaceHolder.addCallback(this);
+		image = (ImageView) findViewById(R.id.camera_image);
+		tag = (ImageView) findViewById(R.id.camera_tag);
+		as = new AnimationSet(false);
+
+		sa = new ScaleAnimation(1.0f, 0.12f, 1.0f, 0.12f);
+		sa.setDuration(1000);
+		as.addAnimation(sa);
+
+		ta = new TranslateAnimation(0, 1000, 0, 333);
+		ta.setDuration(1000);
+		as.addAnimation(ta);
+		as.setAnimationListener(this);
 	}
+
+	private PictureCallback jpeg = new PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+			image.setImageBitmap(bitmap);
+			image.setVisibility(View.VISIBLE);
+
+			image.startAnimation(as);
+		}
+	};
 
 	@Override
 	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
@@ -109,7 +153,7 @@ public class CameraActivity extends Activity implements Callback {
 		}
 		mCamera.stopPreview();
 		mCamera.release();
-		mCamera =Camera.open(cameraId);
+		mCamera = Camera.open(cameraId);
 		try {
 			mCamera.setPreviewDisplay(mSurfaceHolder);
 			int degrees = getDisplayOritation(getDispalyRotation(), cameraId);
@@ -120,7 +164,29 @@ public class CameraActivity extends Activity implements Callback {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
 
+	public void save(View v) {
+		if (isAnimating) {
+			return;
+		}
+		isAnimating = true;
+		mCamera.takePicture(null, null, jpeg);
+
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		image.setVisibility(View.GONE);
+		isAnimating = false;
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
 	}
 
 }
